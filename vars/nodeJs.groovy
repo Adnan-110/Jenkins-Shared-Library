c634d54ee3d8fa243358ad07807225ce1c95a554
Adnan <adnanshaikh11501@gmail.com>
2023-12-15 00:52:40 +0530

    Jenkins/groovy embed

---
+++ vars/nodeJs.groovy
@@ -0,0 +1,6 @@
+lintChecks() {
+    sh "echo ****** Starting Style Checks ****** "
+    sh "npm install jslint"
+    sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
+    sh "echo ****** Style Check are Completed ******"
+}

f84194538dd9e085b37c0c791736687927725445
Adnan <adnanshaikh11501@gmail.com>
2023-12-15 01:56:10 +0530

    Jenkins/groovy embed error check

--- vars/nodeJs.groovy
+++ vars/nodeJs.groovy
@@ -1,10 +1,10 @@
-def lintChecks(String component) {
+def lintChecks() {
     sh """echo ****** Starting Style Checks for ${component} ****** """
     sh "npm install jslint"
     sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
     sh """echo ****** Style Check are Completed for ${component} ******"""
 }
-def call(String component) {
+def call() {
         pipeline{
             agent{
                 label 'ws'

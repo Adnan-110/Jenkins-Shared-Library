6c9b935014b1e4b7e72ef19bc036ccf018ef47f4
Adnan <adnanshaikh11501@gmail.com>
2023-12-15 01:26:29 +0530

    Jenkins/groovy embed

--- vars/nodeJs.groovy
+++ vars/nodeJs.groovy
@@ -1,6 +1,28 @@
-def lintChecks() {
-    sh "echo ****** Starting Style Checks ****** "
+def lintChecks(String component) {
+    sh "echo ****** Starting Style Checks for ${component} ****** "
     sh "npm install jslint"
     sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
-                sh "echo ****** Style Check are Completed ******"
+                sh "echo ****** Style Check are Completed for ${component} ******"
+}
+def call(String component) {
+        pipeline{
+            agent{
+                label 'ws'
+        }
+        stages{
+            stage('Lint Checks') {
+                steps {
+                    script{
+                        helloWorld.info(component)
+                        nodeJs.lintChecks(component)
+                    }
+                }
+            }
+            stage('Static Code Analysis') {
+                steps{
+                    sh "echo ****** Starting Static Code Analysis ******"
+                }   
+            }
+        }
+    }
 }

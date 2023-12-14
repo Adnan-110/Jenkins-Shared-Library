c282779f0718ed0bbb2ad19dc44a27d0ee247937
Adnan <adnanshaikh11501@gmail.com>
2023-12-15 02:30:48 +0530

    Jenkins/groovy env variable

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,9 @@
+// def info() {
 def info(message) {
     echo "Hello World....!"
     echo "The Component is ${message} "
+    // echo "The Component is ${COMPONENT} "
+
 }
 
-info()
+// info()
--- vars/nodeJs.groovy
+++ vars/nodeJs.groovy
@@ -1,9 +1,14 @@
 def lintChecks(String component) {
     sh """echo ****** Starting Style Checks for ${component} ****** """
+    //  sh "echo ****** Starting Style Checks for ${COMPONENT} ******"
     sh "npm install jslint"
     sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
     sh """echo ****** Style Check are Completed for ${component} ******"""
+    //  sh "echo ****** Style Check are Completed for ${COMPONENT} ******"
+    // We have used environment variable directly
 }
+// def call() {
+// Above we are not catching the parameter value
 def call(String component) {
         pipeline{
             agent{
@@ -15,6 +20,8 @@ def call(String component) {
                     script{
                         helloWorld.info(component)
                         lintChecks(component)
+                        // helloWorld.info()
+                        // lintChecks()
                     }
                 }
             }

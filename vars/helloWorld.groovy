6a771e5971a3906d35d0bbc204a70ce422e54ff5
Adnan <adnanshaikh11501@gmail.com>
2023-12-15 02:36:07 +0530

    Jenkins/groovy env variable

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,8 +1,8 @@
-// def info() {
-def info(message) {
+def info() {
+// def info(message) {
     echo "Hello World....!"
-    echo "The Component is ${message} "
-    // echo "The Component is ${COMPONENT} "
+    // echo "The Component is ${message} "
+    echo "The Component is ${COMPONENT} "
 
 }
 
--- vars/nodeJs.groovy
+++ vars/nodeJs.groovy
@@ -1,15 +1,15 @@
 def lintChecks(String component) {
-    sh """echo ****** Starting Style Checks for ${component} ****** """
-    //  sh "echo ****** Starting Style Checks for ${COMPONENT} ******"
+    // sh """echo ****** Starting Style Checks for ${component} ****** """
+     sh "echo ****** Starting Style Checks for ${COMPONENT} ******"
     sh "npm install jslint"
     sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
-    sh """echo ****** Style Check are Completed for ${component} ******"""
-    //  sh "echo ****** Style Check are Completed for ${COMPONENT} ******"
+    // sh """echo ****** Style Check are Completed for ${component} ******"""
+     sh "echo ****** Style Check are Completed for ${COMPONENT} ******"
     // We have used environment variable directly
 }
-// def call() {
+def call() {
 // Above we are not catching the parameter value
-def call(String component) {
+// def call(String component) {
         pipeline{
             agent{
                 label 'ws'
@@ -18,10 +18,10 @@ def call(String component) {
             stage('Lint Checks') {
                 steps {
                     script{
-                        helloWorld.info(component)
-                        lintChecks(component)
-                        // helloWorld.info()
-                        // lintChecks()
+                        // helloWorld.info(component)
+                        // lintChecks(component)
+                        helloWorld.info()
+                        lintChecks()
                     }
                 }
             }

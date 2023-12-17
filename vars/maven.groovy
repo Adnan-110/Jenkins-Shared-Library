87806925881cf37f10410897bbd560f585b1747e
Adnan <adnanshaikh11501@gmail.com>
2023-12-17 16:27:46 +0530

    error resolvation

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -23,6 +23,9 @@ def call() {
         agent{
             label 'ws'
         }
+        tools{  // This option will make build tools available only for this single run and will not install permanently.
+            maven 'maven-390' // In This way we configured in Jenkins->mangage->Tools section that whenever maven-390 is passed make maven-3.9.0 version available
+        }
         stages{
             stage('Lint Checks') {
                 steps {

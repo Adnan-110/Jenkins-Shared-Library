75cb39b942202ebf963a7dc3a45914430443f432
Adnan <adnanshaikh11501@gmail.com>
2023-12-17 13:53:47 +0530

    Modification env variable

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,8 @@
 def sonarChecks() {
     echo "****** Starting Static Code Analysis for ${COMPONENT} ******"    
-    sh """
+    sh '''
     sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
-    """
+    '''
     echo "****** Static Code Analysis is Completed for ${COMPONENT} ******"
 
     }

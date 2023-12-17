24a50116c5da7fada4312b8434ade3613dc48461
Adnan <adnanshaikh11501@gmail.com>
2023-12-18 00:48:38 +0530

    sonarscanner commented

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,6 @@
 def sonarChecks() {
     echo "****** Starting Static Code Analysis for ${COMPONENT} ******"    
-    sh '''
-    sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}
-    '''
+    //sh '''sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}'''
     echo "****** Static Code Analysis is Completed for ${COMPONENT} ******"
 
     }

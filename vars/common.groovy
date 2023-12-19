629442edd040c6fefe6d32647646a8d647dbf7d7
Adnan <adnanshaikh11501@gmail.com>
2023-12-20 04:45:21 +0530

    Scripted Pipeline

--- vars/common.groovy
+++ vars/common.groovy
@@ -146,9 +146,9 @@ def generatingArtifacts() {
 
         stage('Uploading the Artifacts to Nexus') {
             withCredentials([usernamePassword(credentialsId: 'NEXUS_CRED', passwordVariable: 'NEXUS_CRED_PSW', usernameVariable: 'NEXUS_CRED_USR')]) {
-                echo "Uploading the ${COMPONENT} Artifacts"
-                sh "curl -f -v -u ${NEXUS_CRED_USR}:${NEXUS_CRED_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
-                echo "Upload of ${COMPONENT} Artifacts Completed"
+                sh "echo Uploading the ${COMPONENT} Artifacts to Nexus"
+                sh "curl -f -v -u ${NEXUS_CRED_USR}:${NEXUS_CRED_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+                sh "echo Upload of ${COMPONENT} Artifacts to Nexus Completed"
             }
         }
     

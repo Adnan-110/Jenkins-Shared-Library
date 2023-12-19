c722fe566310a29a96ffea88c2defdca4115b6c4
Adnan <adnanshaikh11501@gmail.com>
2023-12-19 18:40:41 +0530

    Scripted Pipeline

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,21 +9,21 @@ def lintChecks() {
     stage('Lint Checks') {
         if (env.APP_TYPE ==  "python"){
              sh '''echo ****** Starting Style Checks for ${COMPONENT} ****** 
-                # sh "pip3 install pylint"
-                # sh "pylint *.py || true"
-                echo ****** Style Check are Completed for ${COMPONENT} ****** 
-            '''
+                #  pip3 install pylint
+                #  pylint *.py || true
+                   echo ****** Style Check are Completed for ${COMPONENT} ****** 
+             '''
         }
         else if (env.APP_TYPE == "maven"){
             sh '''echo ****** Starting Style Checks for ${COMPONENT} ****** 
-                 # mvn checkstyle:check || true
+               #  mvn checkstyle:check || true
                   echo ****** Style Check are Completed for ${COMPONENT} ****** 
             '''
         }
         else if (env.APP_TYPE == "node"){
             sh '''echo ****** Starting Style Checks for ${COMPONENT} ****** 
-                  npm install jslint
-                  /home/centos/node_modules/jslint/bin/jslint.js server.js || true
+               # npm install jslint
+               # /home/centos/node_modules/jslint/bin/jslint.js server.js || true
                   echo ****** Style Check are Completed for ${COMPONENT} ****** 
             '''
         }
